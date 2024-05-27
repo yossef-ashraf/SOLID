cb582964a0375c461901006178149ed3ea083c17
yossef-ashraf <yossefff2001@gmail.com>
2024-05-27 09:56:39 +0300

    code

---
+++ SingleResponsibility.php
@@ -0,0 +1,50 @@
+<?php 
+
+// Single Responsibility
+// Role
+// 1. Class should have only one reason to change
+// 2. Class should have only one responsibility
+// 3. Class should have only one job
+
+// First Example
+class User {
+    public function register() {
+        // Register the user
+    }
+
+    public function login() {
+        // Login the user
+    }
+}
+
+class login{
+
+}
+
+class register{
+    
+}
+
+class SingleResponsibilityUser{
+
+    private $log;
+    private $register;
+
+    public function __construct($log, $register){
+        $this->log = $log;
+        $this->register = $register;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
