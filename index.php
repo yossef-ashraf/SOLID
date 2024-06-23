67474ceaaf796a2040014bed2d65fa840bf8a64e
yossef-ashraf <yossefff2001@gmail.com>
2024-06-23 11:28:19 +0300

    feat: Add Open Close Principle implementation
    The code changes introduce the Open Close Principle implementation. New files and directories have been added to support this principle. The `PlayerInterfaces` interface defines a `player` method, and the `AttackPlayer` and `DefencePlayer` classes implement this interface. The `Matchs` class uses these player classes to demonstrate the Open Close Principle. The `Roles.txt` file provides a brief explanation of the principle.
    
    Note: The recent user commits and repository commits do not provide any additional context for the commit message.

--- index.php
+++ index.php
@@ -4,3 +4,12 @@ require 'vendor/autoload.php';
 
 
 // $SingleResponsibilityUser= new APP\SingleResponsibility\SingleResponsibilityUser();
+
+
+$players = ["Defence","Defence","Defence","Defence","Attack","Attack","Attack"]; // can add more players useing interface
+$OpenClose= new APP\OpenClose\Matchs($players);
+
+
+
+
+
