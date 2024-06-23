0c8fbc5f3ad95ec0dcf220198f684ead1c396ea0
yossef-ashraf <yossefff2001@gmail.com>
2024-06-24 02:15:23 +0300

    Liskov Substitution Principle

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ require 'vendor/autoload.php';
 // $SingleResponsibilityUser= new APP\SingleResponsibility\SingleResponsibilityUser();
 
 
-$players = ["Defence","Defence","Defence","Defence","Attack","Attack","Attack"]; // can add more players useing interface
-$OpenClose= new APP\OpenClose\Matchs($players);
+// $players = ["Defence","Defence","Defence","Defence","Attack","Attack","Attack"]; // can add more players useing interface
+// $OpenClose= new APP\OpenClose\Matchs($players);
 
 
-
+$Liskov= new APP\Liskov\Liskov();
 
 
